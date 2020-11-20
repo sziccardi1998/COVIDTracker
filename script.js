742cc8ef4ebb69b938a04fac4d162408591a397a
sziccardi1998 <sziccardifullstack@gmail.com>
2020-11-19 21:15:29 -0500

    addition of geoCoding.

--- script.js
+++ script.js
@@ -25,3 +25,31 @@ $.ajax({
 }).then(function(response){
     console.log(response);
 })
+
+// get user location from browser
+function getLocation() {
+    if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(showPosition);
+    }
+    else{
+        // display a message about using a browser with geolocation
+    }
+}
+
+function showPosition(position) {
+    var lat = position.coords.latitude;
+    var lon = position.coords.longitude;
+    // pass to function that uses ajax call and geolocater to find the users country
+    reverseGeoCode(lat, lon);
+}
+
+function reverseGeoCode(lat, lon){
+    var locationIQ = "https://us1.locationiq.com/v1/reverse.php?key=pk.d153eede84e6d6d8954c160de6babc21&lat=" + lat + "&lon=" + lon + "&format=json";
+    $.ajax({
+        url: locationIQ,
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
+        var userCountry = response.address.country;
+    })
+}
